454b37f0bdc48c804c7b60eaaeb12b6481aa7d21
YUNGANG WU <yunganw@amazon.com>
2020-09-03 10:02:25 +1000

    first commit

---
+++ client_creds_grant.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#===============================================================================
+# CLIENT CREDENTIALS GRANT
+#===============================================================================
+
+## Set constants ##
+AUTH_DOMAIN="YOURDOMAIN.auth.REGION.amazoncognito.com" # Update MYDOMAIN and REGION
+CLIENT_ID="xxxxxxxxxxxxxx" # Replace with app client ID
+CLIENT_SECRET="xxxxxxxxxx" # Replace with app client secret
+GRANT_TYPE="client_credentials"
+
+## Get access token from /oauth2/token endpoint ##
+authorization="$(printf "${CLIENT_ID}:${CLIENT_SECRET}" \
+                    | base64 \
+                    | tr -d "\n"
+                )"
+curl "https://${AUTH_DOMAIN}/oauth2/token" \
+    -H "Authorization: Basic ${authorization}" \
+    -d "grant_type=${GRANT_TYPE}"
